0ee83609faed4d0703bd5c6822e4810663039ae0
caijx <caijx@cai7>
2016-11-28 22:14:06 +0800

    2

--- src/main/webapp/view/demo/js/app.js
+++ src/main/webapp/view/demo/js/app.js
@@ -1,9 +1,45 @@
 (function() {
     'use strict';
-    var myModule = angular.module("HelloAngular", []);
+    var myModule = angular.module("HelloAngular", ["isteven-multi-select"]);
     myModule.controller("helloAngular", [ "$scope", function($scope) {
         $scope.greeting = {
             text : "hello",
         };
+        
+        $scope.modernBrowsers = [
+                {
+                    icon : '<img src="https://cdn1.iconfinder.com/data/icons/fatcow/32/opera.png" />',
+                    name : 'Opera',
+                    maker : 'Opera Software',
+                    ticked : false
+                },
+                {
+                    icon : '<img  src="https://cdn1.iconfinder.com/data/icons/fatcow/32/internet_explorer.png" />',
+                    name : 'Internet Explorer',
+                    maker : 'Microsoft',
+                    ticked : false
+                },
+                {
+                    icon : '<img  src="https://cdn1.iconfinder.com/data/icons/humano2/32x32/apps/firefox-icon.png" />',
+                    name : 'Firefox',
+                    maker : 'Mozilla Foundation',
+                    ticked : true
+                },
+                {
+                    icon : '<img  src="https://cdn1.iconfinder.com/data/icons/fatcow/32x32/safari_browser.png" />',
+                    name : 'Safari',
+                    maker : 'Apple',
+                    ticked : false
+                },
+                {
+                    icon : '<img  src="https://cdn1.iconfinder.com/data/icons/google_jfk_icons_by_carlosjj/32/chrome.png" />',
+                    name : 'Chrome',
+                    maker : 'Google',
+                    ticked : false
+                } ];
+        
+        $scope.outputBrowsers = [
+             {   icon: "www",   name: "eeee",   maker: "qqq", ticked: true  },
+         ];
     } ]);
 })();
